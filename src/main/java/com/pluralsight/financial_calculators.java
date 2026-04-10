8313f5e2e55ad4a3f13332eb5375a0f9d06d723a
bryan <bryanmendez21@icloud.com>
2026-04-10 13:15:02 -0400

    Implemented compounded interest formula and a total interest formula to get monthly payments and total interest.

--- src/main/java/com/pluralsight/financial_calculators.java
+++ src/main/java/com/pluralsight/financial_calculators.java
@@ -13,16 +13,22 @@ public class financial_calculators {
         if (pick_calculator == 1){
             // asking for loan variables
             System.out.print("What is the principle/loan amount: ");
-            int principle = data_catcher.nextInt();
+            double principle = data_catcher.nextInt();
             System.out.print("What is the interest rate: ");
-            float interest_rate = data_catcher.nextFloat();
-            System.out.print("How long is the loan in months: ");
+            double interest_rate = data_catcher.nextDouble();
+            System.out.print("How long is the loan in years: ");
             int loan_length = data_catcher.nextInt();
+
+            double total_months = 12 * loan_length;
+            // returning these values to the method
+            double monthly_payment = morgage(principle,interest_rate,loan_length, total_months);
+            // Total interest logic
+            double total_interest = (monthly_payment * total_months) - principle;
             // Outcome
-            //System.out.printf("Your monthly payment is: $%.2f",  );
+            System.out.printf("Your monthly payment is: $%.2f \nYour total interest is: $%.2f" ,monthly_payment, total_interest);
 
         }
-        else if (pick_calculator == 2){
+       /* else if (pick_calculator == 2){
 
         }
         else if (pick_calculator == 3){
@@ -30,13 +36,17 @@ public class financial_calculators {
         }
         else {
             System.out.println("Pick from 1, 2 or 3");
-        }
+        }*/
 
     }
 
     // Mortgage calculator
-    public static float morgage(int principle, float interest_rate, int loan_length) {
-        return  ;
+    public static double morgage(double principle, double interest_rate, int loan_length, double total_months) {
+        // Monthly interest rate
+        double monthly_rate = (interest_rate/100) / 12; // interest divided by months in a year
+        double length_of_month_rate = Math.pow(1 + monthly_rate,total_months); //
+        // Formula M = p * ( i * ( 1 + i)^n / ( (1 + i)^n ) -1)
+        return principle * ( monthly_rate * (length_of_month_rate)/ ((length_of_month_rate) -1) );
 
 
     }
